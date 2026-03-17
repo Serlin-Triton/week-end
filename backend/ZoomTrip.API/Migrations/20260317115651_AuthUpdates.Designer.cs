f35a8d88a2fb88b2ab08529ef30637407c87ad40
MathanKumarK <MathanKumarK@DESKTOP-Q99P3P4>
2026-03-17 17:31:23 +0530

    alll ok

--- backend/ZoomTrip.API/Migrations/20260317115651_AuthUpdates.Designer.cs
+++ backend/ZoomTrip.API/Migrations/20260317115651_AuthUpdates.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ZoomTrip.API.Data;
@@ -11,9 +12,11 @@ using ZoomTrip.API.Data;
 namespace ZoomTrip.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260317115651_AuthUpdates")]
+    partial class AuthUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
